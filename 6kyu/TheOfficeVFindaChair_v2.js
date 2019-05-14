3ddd089098ffdf973ce4bf0c5dfef75ebd793b6d
Thilo Sperber <t.sperber@gmx.net>
2019-05-14 11:27:46 +0200

    Completed TheOfficeVFindaChair

---
+++ 6kyu/TheOfficeVFindaChair_v2.js
@@ -0,0 +1,18 @@
+function meeting(x, need) {
+  if (need === 0) return 'Game On';
+  let result = [];
+  let stillNeed = need;
+  for (let i = 0; i < x.length; i++) {
+    availableChairs = x[i][1] - x[i][0].length;
+    if (availableChairs > 0) {
+      if (result.length > 0)
+        stillNeed = need - result.reduce((acc, chairs) => acc + chairs);
+      if (availableChairs < stillNeed) result.push(availableChairs);
+      else {
+        result.push(stillNeed);
+        return result;
+      }
+    } else result.push(0);
+  }
+  return 'Not enough!';
+}
